271501816767dd05760cfe6ebc95362fd99ca0b6
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-02-12 09:33:28 +0700

    Mix truffle in reading 'contract'

---
+++ truffle.js
@@ -0,0 +1,12 @@
+module.exports = {
+	networks: {
+		// See <http://truffleframework.com/docs/advanced/configuration>
+	  // to customize your Truffle configuration!
+	  development: {
+	      // host: 'docker.for.mac.localhost',
+	      host: 'localhost',
+	      port: 8545,
+	      network_id: '*',
+	  }
+	}
+};
